ffde9d98f79ff79e77821eac0cc83e620fe5e70b
xiaowang <xiaowang@szzbmy.com>
2019-01-18 17:59:01 +0800

    。

--- springboot-study/src/main/java/com/xw/springbootstudy/MySpringBootServletInitializer.java
+++ springboot-study/src/main/java/com/xw/springbootstudy/MySpringBootServletInitializer.java
@@ -6,6 +6,7 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 /**
  * 打成war包启动，servlet3.0后特性才支持（以及META-INF/services（spring-web下有）下的源数据）。
  * jar启动时，虽然嵌入式的容器也是在refresh中启动的，但是这个特性是失效的。
+ * 		（因为此时tomcat不是自己启动的，而是spring调用内嵌tomcat的start方法启动的）
  */
 public class MySpringBootServletInitializer extends SpringBootServletInitializer {
 	@Override
--- springboot-study/src/main/java/com/xw/springbootstudy/applicationlistener/MyApplicationListener.java
+++ springboot-study/src/main/java/com/xw/springbootstudy/applicationlistener/MyApplicationListener.java
@@ -1,4 +1,44 @@
 package com.xw.springbootstudy.applicationlistener;
 
-public class MyApplicationListener {
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.ContextStartedEvent;
+import org.springframework.context.event.ContextStoppedEvent;
+import org.springframework.stereotype.Component;
+
+/**
+ * ContextRefreshedEvent
+ * ApplicationContext 被初始化或刷新时，该事件被发布。这也可以在 ConfigurableApplicationContext 接口中使用 refresh() 方法来发生。
+ *
+ * ContextStartedEvent
+ * 当使用 ConfigurableApplicationContext 接口中的 start() 方法启动 ApplicationContext 时，该事件被发布。你可以调查你的数据库，或者你可以在接受到这个事件后重启任何停止的应用程序。
+ *
+ * ContextStoppedEvent
+ * 当使用 ConfigurableApplicationContext 接口中的 stop() 方法停止 ApplicationContext 时，发布这个事件。你可以在接受到这个事件后做必要的清理的工作。
+ *
+ * ContextClosedEvent
+ * 当使用 ConfigurableApplicationContext 接口中的 close() 方法关闭 ApplicationContext 时，该事件被发布。一个已关闭的上下文到达生命周期末端；它不能被刷新或重启。
+ *
+ * RequestHandledEvent
+ * 这是一个 web-specific 事件，告诉所有 bean HTTP 请求已经被服务。
+ */
+
+@Component
+public class MyApplicationListener implements ApplicationListener<ApplicationEvent> {
+    @Override
+    public void onApplicationEvent(ApplicationEvent event) {
+        if (event instanceof ContextClosedEvent) {
+            System.out.println(event.getClass().getSimpleName() + " 事件已发生！");
+        } else if (event instanceof ContextRefreshedEvent) {
+            System.out.println(event.getClass().getSimpleName() + " 事件已发生！");
+        } else if (event instanceof ContextStartedEvent) {
+            System.out.println(event.getClass().getSimpleName() + " 事件已发生！");
+        } else if (event instanceof ContextStoppedEvent) {
+            System.out.println(event.getClass().getSimpleName() + " 事件已发生！");
+        } else {
+            System.out.println(event.getClass().getSimpleName() + " 事件已发生！");
+        }
+    }
 }
